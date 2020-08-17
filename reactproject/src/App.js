26109154f7b34f2e472b498c1a977c1e8f361e80
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2020-08-17 19:05:11 +0530

    Person commit

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>HI</h1>
+        <Person></Person>
       </div>
     )
   }
