1e261a3a0fd83e7954ad4b4b7b9235dcf5f6aa85
Vishnu Naga Praveen Deepthimahanthi <videepth@microsoft.com>
2020-08-17 20:43:42 +0530

    func instead of class

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Person from './Person/Person'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>HI</h1>
-        <Person name="Max" age="28">
-          Vishnu is good
-        </Person>
-      </div>
-    )
+const app = (props) => {
+  const [personState, setPersonState] = useState({
+    persons: [
+      { name: 'MAXd', age: 28 },
+      { name: 'RAM', age: 28 },
+      { name: 'SHYAM', age: 28 },
+    ],
+  })
+
+  const switchNameHandler = () => {
+    // console.log('clicked')
+    setPersonState({
+      persons: [
+        { name: 'MAXdy', age: 25 },
+        { name: 'RAM', age: 28 },
+        { name: 'SHYAM', age: 28 },
+      ],
+    })
   }
+
+  return (
+    <div className="App">
+      <h1>HI</h1>
+      <button onClick={switchNameHandler}>Switch</button>
+      <Person name={personState.persons[0].name} age="28">
+        Vishnu is good
+      </Person>
+    </div>
+  )
 }
 
-export default App
+export default app
